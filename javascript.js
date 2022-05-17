23a314feced9376840f66ce9832c6f286cbf842f
Justin Morrow <morrowjustin19@gmail.com>
2022-05-16 19:13:16 -0700

    updating

--- javascript.js
+++ javascript.js
@@ -1,13 +1,31 @@
 // Global variables
 var cityHistory = [];
 var weatherApiUrl= 'https://api.openweathermap.org';
-var ApiKey = 'fd5835085c5c060761fc94438b898ef';
+var ApiKey = 'ffd5835085c5c060761fc94438b898ef';
+// var ApiKey = '452691537bbf409c6544c02c76a1f6ad';
 
 var cityForm = document.querySelector('#city-form');
 var cityInput = document.querySelector('#city-input');
-function rendercityHistory() {
-    // TBD
-    
+var todayContainer = document.getElementById('today');
+var cityHistoryContainer = document.getElementById('city-history');
+// var forecastContainer = document.querySelector('#5day-forecast');
+// Add timezone plugins to day.js
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+function displaycityHistory() {
+   // Start at end of history array and count down to show the most recent at the top.
+   for (var i = cityHistory.length - 1; i >= 0; i--) {
+    var btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-controls', 'today forecast');
+    btn.classList.add('history-btn', 'btn-history');
+
+    // `data-search` allows access to city name when click handler is invoked
+    btn.setAttribute('data-search', cityHistory[i]);
+    btn.textContent = cityHistory[i];
+    cityHistoryContainer.append(btn);
+   }  
 }
 function appendToHistory(city) {
     // If there is no search term return the function
@@ -17,17 +35,85 @@ function appendToHistory(city) {
     cityHistory.push(city);
   
     localStorage.setItem('city-history', JSON.stringify(cityHistory));
-    rendercityHistory();
+    displaycityHistory();
   }
-  function renderCurrentWeather (city, current, timezone) {
-    //   TBD
+  function displayCurrentWeather (city, current, timezone) {
+    var date = dayjs().tz(timezone).format('M/D/YYYY');
+    // Store response data from our fetch request in variables
+    var tempF = current.temp;
+    var windMph = current.wind_speed;
+    var humidity = current.humidity;
+    var uvi = current.uvi;
+    var iconUrl = `https://openweathermap.org/img/w/${current.weather[0].icon}.png`;
+    var iconDescription = current.weather[0].description || weather[0].main;
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var heading = document.createElement('h2');
+    var weatherIcon = document.createElement('img');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+    var uvEl = document.createElement('p');
+    var uviBadge = document.createElement('button');
+    card.setAttribute('class', 'card');
+    cardBody.setAttribute('class', 'card-body');
+    card.append(cardBody);
+    heading.setAttribute('class', 'h3 card-title');
+    tempEl.setAttribute('class', 'card-text');
+    windEl.setAttribute('class', 'card-text');
+    humidityEl.setAttribute('class', 'card-text');
+    heading.textContent = `${city} (${date})`;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    weatherIcon.setAttribute('class', 'weather-img');
+    heading.append(weatherIcon);
+    tempEl.textContent = `Temp: ${tempF}°F`;
+    windEl.textContent = `Wind: ${windMph} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+    uvEl.textContent = 'UV Index: ';
+    uviBadge.classList.add('btn', 'btn-sm');
+    if (uvi < 3) {
+      uviBadge.classList.add('btn-success');
+    } else if (uvi < 7) {
+      uviBadge.classList.add('btn-warning');
+    } else {
+      uviBadge.classList.add('btn-danger');
+    }
+    uviBadge.textContent = uvi;
+    uvEl.append(uviBadge);
+    cardBody.append(uvEl);
+    todayContainer.innerHTML = '';
+    todayContainer.append(card);
   }
-  function renderForecast (daily, timezone) {
-      //TBD
+  function displayForecast (daily, timezone) {
+    return;
+      // Create unix timestamps for start and end of 5 day forecast
+  var startDt = dayjs().tz(timezone).add(1, 'day').startOf('day').unix();
+  var endDt = dayjs().tz(timezone).add(6, 'day').startOf('day').unix();
+
+  // var headingCol = document.createElement('div');
+  // var heading = document.createElement('h4');
+
+  // headingCol.setAttribute('class', 'col-12');
+  // heading.textContent = '5-Day Forecast:';
+  // headingCol.append(heading);
+
+  // forecastContainer.innerHTML = '';
+  // forecastContainer.append(headingCol);
+  for (var i = 0; i < dailyForecast.length; i++) {
+    // The api returns forecast data which may include 12pm on the same day and
+    // always includes the next 7 days. The api documentation does not provide
+    // information on the behavior for including the same day. Results may have
+    // 7 or 8 items.
+    if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
+      displayForecastCard(dailyForecast[i], timezone);
+    }
+  }
   }
 function renderPage (city, data) {
-        renderCurrentWeather(city, data.current, data.timezone);
-        renderForecast(data.daily, data.timezone);
+        displayCurrentWeather(city, data.current, data.timezone);
+        displayForecast(data.daily, data.timezone);
       }
 
 function getWeather(location) {
